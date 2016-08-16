8e1e2a301399a1b14ffc885ff5b45cec5f736f7f
Jose Marbin Tapia Rodriguez <jtapia@indestim.es>
2016-08-17 01:08:41 +0200

    add WeatherServiceDelegate class

---
+++ weatherapp/WeatherServiceDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherServiceDelegate.swift
+//  weatherapp
+//
+//  Created by jose marbin tapia rodriguez on 17/8/16.
+//  Copyright © 2016 jose marbin tapia rodriguez. All rights reserved.
+//
+
+import Foundation
+
+protocol WeatherServiceDelegate {
+    func setWeather()
+    func error()
+}
