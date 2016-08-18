eeeb381e3b90694d948daa2b4983e03cb112f3d6
Jose Marbin Tapia Rodriguez <jtapia@indestim.es>
2016-08-18 20:34:18 +0200

    add weather class

---
+++ weatherapp/Weather.swift
@@ -0,0 +1,9 @@
+//
+//  Weather.swift
+//  weatherapp
+//
+//  Created by jose marbin tapia rodriguez on 18/8/16.
+//  Copyright © 2016 jose marbin tapia rodriguez. All rights reserved.
+//
+
+import Foundation
